906e07ded3ab6e05122021dcc939c3e663b87418
xiaohe123456 <862000954@qq.com>
2022-07-20 14:31:07 +0800

    对代码进行修改，对淄博山水项目的点云数据进行补充

--- carTail/imgToPcl.cpp
+++ carTail/imgToPcl.cpp
@@ -6,58 +6,24 @@ using namespace cv;
 
 int main()
 {
-	int width = 439;		//变换后图像大小
-	int height = 570;
+	//点云中没有但图像中有的部分的区域大小
+	int width = 672;		
+	int height = 354;
+	//将像素点转换为三维点
 	ofstream points;
 	points.open("points.txt", ios::out);
 	Point3d point;
-
-	//point.x = 0.0458149;
-	//point.z = 0.611243;
-	//point.y = 1.84523;
-	point.x = -0.969545;
-	point.z = 3.24914;
-	point.y = 0.447072;
-	points << fixed << setprecision(6) << point.x << " " << point.y << " " << point.z << endl;
-
-	//point.x = -0.311146;
-	//point.z = 0.542593;
-	//point.y = 1.53438;
-	point.x = -0.679361;
-	point.z = 2.34664;
-	point.y = 0.570891;
-	points << fixed << setprecision(6) << point.x << " " << point.y << " " << point.z << endl;
-
-	//point.x = -0.341513;
-	//point.z = 0.371109;
-	//point.y = 1.76527;
-	point.x = -0.627407;
-	point.z = 2.41783;
-	point.y = 0.124808;
-	points << fixed << setprecision(6) << point.x << " " << point.y << " " << point.z << endl;
-
-	//point.x = 0.145784;
-	//point.z = 0.398173;
-	//point.y = 1.67491;
-	point.x = -0.979542;
-	point.z = 3.25341;
-	point.y = 0.597086;
-	points << fixed << setprecision(6) << point.x << " " << point.y << " " << point.z << endl;
-
-	//在计算坐标时，根据实际情况设置x、y、z中的一个值为定值，就是补充一个平面，将图像铺到上面
+	//在计算坐标时，根据实际情况设置x、y、z中的一个值为定值，将图像坐标转换为三维数据
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			//00575
-			//double x = (double)(45.8149 - 311.146) / 568 * j / 1000 + 0.0458149;
-			//00480
-			//double x = (double)(679.028 - 982.262) / 568 * j / 1000 - 0.982262;
-			point.x = -0.979542;
-			//double z = (double)(611.243 - 401.168) / 437 * i / 1000 + 0.401168;
-			double z = (double)(570.891 - 124.808) / 570 * i / 1000 + 0.124808;
-			point.z = z;
-			point.y = (double)(3253.41 - 2346.64) / 439 * j / 1000 + 2.34664;
+			//补充x方向的点  实际宽度/图像中对应部分的像素数目*宽度/1000（单位换算为m）+初始点坐标
+			point.x = (double)4164.2 / 672 * j / 1000 - 3.67777;
+			point.y = 3.31432;	//补充x、z平面，y设为定值
+			//补充z方向的点  实际高度/图像中对应部分的像素数目*高度/1000+初始点坐标
+			point.z = (double)1247.98 / 354 * i / 1000 + 1.22306;
+			//写入文件
 			points << fixed << setprecision(6) << point.x << " " << point.y << " " << point.z << endl;
 		}
 	}
