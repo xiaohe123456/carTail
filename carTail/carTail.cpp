cac7831fc546a204010441771dac7dd0a8a6c884
xiaohe123456 <862000954@qq.com>
2023-04-27 19:35:50 +0800

    补充完整

--- carTail/carTail.cpp
+++ carTail/carTail.cpp
@@ -1,229 +1,122 @@
 ﻿#include <opencv2/opencv.hpp>
-#include <vector>
 #include <iostream>
-#include <string>
-#include <iomanip>
-#include <fstream>  //文件流库函数
-#include <stdio.h>
-#include <stdlib.h>
-
-using namespace std;
+#include <ctype.h>
 using namespace cv;
-
-void SplitString(const string& s, vector<string>& v, const string& c)
+using namespace std;
+int main123450()
 {
-	string::size_type pos1, pos2;
-	pos2 = s.find(c);
-	pos1 = 0;
-	while (string::npos != pos2)
-	{
-		v.push_back(s.substr(pos1, pos2 - pos1));
+    // 首先通过标定板的图像像素坐标以及对应的世界坐标，通过PnP求解相机的R&T//
+    Point2f point;
+    vector<Point2f> boxPoints; //存入像素坐标
 
-		pos1 = pos2 + c.size();
-		pos2 = s.find(c, pos1);
-	}
-	if (pos1 != s.length())
-		v.push_back(s.substr(pos1));
+    // Setting box corners in image
+    //one Point
+    point = Point2f((float)729, (float)23); //640X480
+    boxPoints.push_back(point);
+    
+
+    //two Point
+    point = Point2f((float)1534, (float)135); //640X480
+    boxPoints.push_back(point);
+    
+
+    //three Point
+    point = Point2f((float)1520, (float)767); //640X480
+    boxPoints.push_back(point);
+    
+
+    //four Point
+    point = Point2f((float)759, (float)956); //640X480
+    boxPoints.push_back(point);
+    
+
+    //Setting box corners in real world
+    vector<Point3f> worldBoxPoints;  //存入世界坐标
+    Point3f tmpPoint;
+    tmpPoint = Point3f((float)-4938.41, (float)3620.12, (float)0);
+    worldBoxPoints.push_back(tmpPoint);
+    tmpPoint = Point3f((float)-4991.92, (float)1223.53, (float)0);
+    worldBoxPoints.push_back(tmpPoint);
+    tmpPoint = Point3f((float)-4901.97, (float)1170.78, (float)0);
+    worldBoxPoints.push_back(tmpPoint);
+    tmpPoint = Point3f((float)-4931.1, (float)3644.98, (float)0);
+    worldBoxPoints.push_back(tmpPoint);
+    //camera  intristic///
+    cv::Mat cameraMatrix1 = Mat::eye(3, 3, cv::DataType<double>::type);  //相机内参矩阵
+    cv::Mat distCoeffs1(5, 1, cv::DataType<double>::type);  //畸变参数
+    distCoeffs1.at<double>(0, 0) = -0.401070471240794;
+    distCoeffs1.at<double>(1, 0) = 0.230454752485496;
+    distCoeffs1.at<double>(2, 0) = -0.003874071083139;
+    distCoeffs1.at<double>(3, 0) = 0.001197211137004;
+    distCoeffs1.at<double>(4, 0) = -0.133297293899286;
+
+    //Taken from Mastring OpenCV d
+    double fx = 1.348231749848276e+03;
+    double fy = 1.349403880998824e+03;
+    double cx = 9.890477762727005e+02;
+    double cy = 5.874783711978498e+02;
+    cameraMatrix1.at<double>(0, 0) = fx;
+    cameraMatrix1.at<double>(0, 1) = 0.188888118498424;
+    cameraMatrix1.at<double>(1, 1) = fy;
+    cameraMatrix1.at<double>(0, 2) = cx;
+    cameraMatrix1.at<double>(1, 2) = cy;
+
+    //PnP solve R&T///
+    cv::Mat rvec1(3, 1, cv::DataType<double>::type);  //旋转向量
+    cv::Mat tvec1(3, 1, cv::DataType<double>::type);  //平移向量
+    cv::solvePnP(worldBoxPoints, boxPoints, cameraMatrix1, distCoeffs1, rvec1, tvec1, false, CV_ITERATIVE);
+    cv::Mat rvecM1(3, 3, cv::DataType<double>::type);  //旋转矩阵
+    cv::Rodrigues(rvec1, rvecM1);
+
+    // 此处用于求相机位于坐标系内的旋转角度, 2D - 3D的转换并不用求
+    const double PI = 3.1415926;
+    double thetaZ = atan2(rvecM1.at<double>(1, 0), rvecM1.at<double>(0, 0)) / PI * 180;
+    double thetaY = atan2(-1 * rvecM1.at<double>(2, 0), sqrt(rvecM1.at<double>(2, 1) * rvecM1.at<double>(2, 1)
+        + rvecM1.at<double>(2, 2) * rvecM1.at<double>(2, 2))) / PI * 180;
+    double thetaX = atan2(rvecM1.at<double>(2, 1), rvecM1.at<double>(2, 2)) / PI * 180;
+    cout << "theta x  " << thetaX << endl << "theta Y: " << thetaY << endl << "theta Z: " << thetaZ << endl;
+
+    ///根据公式求Zc，即s
+    cv::Mat imagePoint = cv::Mat::ones(3, 1, cv::DataType<double>::type);
+    cv::Mat tempMat, tempMat2;
+    //输入一个2D坐标点，便可以求出相应的s
+    imagePoint.at<double>(0, 0) = 714;
+    imagePoint.at<double>(1, 0) = 23;
+    double zConst = 0;//实际坐标系的距离
+    //计算参数s
+    double s;
+    tempMat = rvecM1.inv() * cameraMatrix1.inv() * imagePoint;
+    tempMat2 = rvecM1.inv() * tvec1;
+    s = zConst + tempMat2.at<double>(2, 0);
+    s /= tempMat.at<double>(2, 0);
+    cout << "s : " << s << endl;
+    ///3D to 2D
+    cv::Mat worldPoints = Mat::ones(4, 1, cv::DataType<double>::type);
+    worldPoints.at<double>(0, 0) = -4938.41;
+    worldPoints.at<double>(1, 0) = 3620.12;
+    worldPoints.at<double>(2, 0) = 2956.16;
+    cout << "world Points :  " << worldPoints << endl;
+    Mat image_points = Mat::ones(3, 1, cv::DataType<double>::type);
+    //setIdentity(image_points);
+    Mat RT_;
+    hconcat(rvecM1, tvec1, RT_);
+    cout << "RT_" << RT_ << endl;
+    image_points = cameraMatrix1 * RT_ * worldPoints;
+    Mat D_Points = Mat::ones(3, 1, cv::DataType<double>::type);
+    D_Points.at<double>(0, 0) = image_points.at<double>(0, 0) / image_points.at<double>(2, 0);
+    D_Points.at<double>(1, 0) = image_points.at<double>(1, 0) / image_points.at<double>(2, 0);
+    //cv::projectPoints(worldPoints, rvec1, tvec1, cameraMatrix1, distCoeffs1, imagePoints);
+    cout << "3D to 2D:   " << D_Points << endl;
+
+    //camera_coordinates
+    Mat camera_cordinates = -rvecM1.inv() * tvec1;
+    // 2D to 3D///
+    cv::Mat imagePoint_your_know = cv::Mat::ones(3, 1, cv::DataType<double>::type); //u,v,1
+    imagePoint_your_know.at<double>(0, 0) = 714;
+    imagePoint_your_know.at<double>(1, 0) = 23;
+    Mat wcPoint = rvecM1.inv() * (cameraMatrix1.inv() * s * imagePoint_your_know - tvec1);
+    Point3f worldPoint(wcPoint.at<double>(0, 0), wcPoint.at<double>(1, 0), wcPoint.at<double>(2, 0));
+    cout << "2D to 3D :" << worldPoint << endl;
+
+    return 0;
 }
-
-int main22()
-{
-
-	//Mat image = imread("D:\\image\\res_1135\\test_00575.jpg");
-	Mat image = imread("D:\\CPP\\carTail\\carTail\\00575.jpg");
-	
-	//Mat perMatrix(3, 3, CV_32F);
-	//float perspective[3][3] = { { 4.1363954544067383e+00, -3.8299959897994995e-01, -1.0489592285156250e+03 }, { -4.7195121645927429e-01, 4.9817070960998535e+00, -8.0866217041015625e+02 }, { 2.0808174667763524e-05, -1.6460096230730414e-03, 1.0 } };
-	//
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	for (int j = 0; j < 3; j++)
-	//	{
-	//		perMatrix.at<float>(i, j) = perspective[i][j];
-	//	}
-	//}
-	//Mat image;
-	//resize(image1, image1, Size(570, 439));
-	//warpPerspective(image1, image, perMatrix, image1.size());
-	//imwrite("perspective.jpg", image);
-	int width = image.cols;
-	int height = image.rows;
-	//对应的二维点和三维点对
-	//float threeDim[4][3] = { { 3.52318, 1.60532, 2.382 }, { 3.64162, 3.09532, 2.48514 }, { 3.75237, 3.01082, 2.04784 }, { 3.74107, 1.02553, 2.0045 } };
-	////float threeDim[4][3] = { { 44.978, 1848.13, 613.271 }, { -275.636, 1684.22, 572.227 }, { 144.249, 1675.99, 400.711 }, { -137.092, 1723.91, 372.779 } };
-	//float threeDim[4][3] = { { -3.67174, 2.47186, 2.28475 }, { -3.65522, 0.851537, 2.47772 }, { -3.56673, 0.809385, 1.30896 }, { -3.75093, 2.99978, 1.35803 } };
-	//float twoDim[4][2] = { { 219.08453, 244.02857 }, { 288.20618, 240.12857 }, { 281.74845, 325.92857 }, { 223.62886, 314.22857 } };
-	//float twoDim[4][2] = { { 10, 6 }, { 483, 7 }, { 481, 326 }, { 12, 401 } };
-	////float twoDim[4][2] = { { 2.39175, 3.342857 }, { 115.52164, 3.9 }, { 115.043298, 181.62857 }, { 2.8701, 223.41428 } };
-	vector<Point3f>outDim;
-	vector<Point2f>inDim;
-	vector<float> distCoeff;
-	distCoeff.push_back(-0.302859416524140);
-	distCoeff.push_back(-1.271153636954774);
-	distCoeff.push_back(1.437499343005666e-04);
-	distCoeff.push_back(0.001155793066997);
-	distCoeff.push_back(9.961442496187518);
-
-	//for (int i = 0; i < 4; i++)
-	//{
-	//	outDim.push_back(Point3f(threeDim[i][0], threeDim[i][1], threeDim[i][2]));
-	//	inDim.push_back(Point2f(twoDim[i][0], twoDim[i][1]));
-	//}
-
-	Mat cameraMatrix(3, 3, CV_32F);
-
-	//float tempMatrix[3][3] = { { 3522.3, 0, 0 }, { 0, 3538.4, 0 }, { 1968.9,1375.4,1.0 } };
-	//float tempMatrix[3][3] = { { 1.457167143613874e+03, 1.958581558298678, 9.610202837075110e+02 }, { 0, 1.457401615094959e+03, 5.454445259345974e+02 }, { 0, 0, 1.0 } };//相机的内参矩阵
-	//float tempMatrix[3][3] = { { 2.198577757644186e+03, 11.191338025179473, 9.764266941199196e+02 }, { 0, 2.207348158784651e+03, 5.715773841932228e+02 }, { 0, 0, 1.0 } };//相机的内参矩阵
-	float tempMatrix[3][3] = { { 2.198577757644186e+03, 11.191338025179473, 9.764266941199196e+02 }, { 0, 2.207348158784651e+03, 5.715773841932228e+02 }, { 0, 0, 1.0 } };//相机的内参矩阵
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cameraMatrix.at<float>(i, j) = tempMatrix[i][j];
-		}
-	}
-
-	Mat cameraMatrixInvertTmp(3, 3, CV_32F);
-	invert(cameraMatrix, cameraMatrixInvertTmp);
-	Mat cameraMatrixInvert(4, 3, CV_32F);
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cameraMatrixInvert.at<float>(i, j) = cameraMatrixInvertTmp.at<float>(i,j);
-		}
-	}
-	cameraMatrixInvert.at<float>(3, 0) = 0;
-	cameraMatrixInvert.at<float>(3, 1) = 0;
-	cameraMatrixInvert.at<float>(3, 2) = 0;
-	//Mat rvec1(1, 3, CV_32F);
-	//Mat	tvec(1, 3, CV_32F);
-	
-	//solvePnP(outDim, inDim, cameraMatrix, distCoeff, rvec1, tvec);
-
-	//Mat rvec;
-	//Rodrigues(rvec1, rvec);
-	//cout << rvec.at<float>(0, 0) << endl;
-	//cout << "旋转" << endl;
-	//cout << rvec << endl;
-	//cout << "平移" << endl;
-	//cout << tvec << endl;
-
-	//[-0.4908487417993784, 0.7426926889754392, -0.4554943275348207;
-	//-0.3682570382042339, 0.2969515762324557, 0.8810258311684764;
-	//0.7895912021337448, 0.6001894126986005, 0.1277435023689902]
-
-	//[216.7156610764334;
-	//-303.5126719731619;
-	//-4149.507534461693]
-
-	//将3*3旋转矩阵和3*1平移矩阵合并为4*4矩阵
-	Mat mTransform(4, 4, CV_32F);
-	//for (int r = 0; r < 3; r++)
-	//{
-	//	for (int c = 0; c < 3; c++)
-	//	{
-	//		mTransform.at<float>(r, c) = rvec.at<float>(r, c);
-	//	}
-	//}
-
-	mTransform.at<float>(0, 0) = -0.4908487417993784;
-	mTransform.at<float>(0, 1) = 0.7426926889754392;
-	mTransform.at<float>(0, 2) = -0.4554943275348207;
-	mTransform.at<float>(0, 3) = 216.7156610764334;
-	mTransform.at<float>(1, 0) = -0.3682570382042339;
-	mTransform.at<float>(1, 1) = 0.2969515762324557;
-	mTransform.at<float>(1, 2) = 0.8810258311684764;
-	mTransform.at<float>(1, 3) = -303.5126719731619;
-	mTransform.at<float>(2, 0) = 0.7895912021337448;
-	mTransform.at<float>(2, 1) = 0.6001894126986005;
-	mTransform.at<float>(2, 2) = 0.1277435023689902;
-	mTransform.at<float>(2, 3) = -4149.507534461693;
-	mTransform.at<float>(3, 0) = 0;
-	mTransform.at<float>(3, 1) = 0;
-	mTransform.at<float>(3, 2) = 0;
-	mTransform.at<float>(3, 3) = 1;
-
-	Mat mTransformInvert(4, 4, CV_32F);
-	invert(mTransform, mTransformInvert);
-
-
-	Mat point(4, 1, CV_32F);
-	Mat test(3, 1, CV_32F);
-
-	ofstream points;
-	points.open("points.txt", ios::out);
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			double x = (double)970 / 232 * j / 1000;
-			test.at<float>(0, 0) = x;
-			double y = (double)490 / 273 * i / 1000;
-			test.at<float>(1, 0) = y;
-			test.at<float>(2, 0) = 1;
-			//cout << test.at<float>(0, 0) << endl;
-			//cout << test.at<float>(1, 0) << endl;
-			point = mTransformInvert * cameraMatrixInvert * test;
-			//cout << fixed << setprecision(10) << "point0 = " << point.at<float>(0, 0) << endl;
-			points << fixed << setprecision(10) << point.at<float>(0, 0) << " " << point.at<float>(2, 0) << " " << point.at<float>(1, 0) << endl;
-			//cout << point << endl;
-		}
-	}
-	points.close();
-
-	////读取txt文件
-	//ifstream infile;
-	//infile.open("1.txt");		//将文件流对象与文件连接起来 
-	//assert(infile.is_open());   //若失败,则输出错误消息,并终止程序运行 
-
-	//string s;
-	//vector<string> v;
-	//vector<double> tmp;
-	//vector<vector<double>> all_data;
-	////	all_data.resize(all_data.max_size() + 1);
-	//while (getline(infile, s))
-	//{
-	//	//cout << s << endl;
-	//	SplitString(s, v, " "); //可按多个字符来分隔;
-	//	for (int i = 0; i < 3; i++)
-	//	{
-	//		tmp.push_back(atof(v[i].c_str()) * 1000);
-	//	}
-	//	all_data.push_back(tmp);
-	//	v.clear();
-	//	tmp.clear();
-	//}
-
-	//infile.close();             //关闭文件输入流 
-	///*for (int i = 0; i < all_data.size(); i++)
-	//{
-	//cout << all_data[i][0] << " " << all_data[i][1] << " " << all_data[i][2] << endl;
-	//}*/
-	//cout << all_data.size() << endl;
-	//vector<Point3f> all_points;
-	//for (int i = 0; i < all_data.size(); i++)
-	//{
-	//	all_points.push_back(Point3f(all_data[i][0], all_data[i][1], all_data[i][2]));
-	//}
-
-	//std::vector<cv::Point2f> projectedPoints;
-	//cv::projectPoints(all_points, rvec, tvec, cameraMatrix, distCoeff, projectedPoints);
-	//for (int i = 0; i < projectedPoints.size(); i++)
-	//{
-	//	Point2f p = projectedPoints[i];
-	//	if (p.y <= 1080)
-	//	{
-	//		circle(image, p, 5, Scalar(255, 255, 0), 1, 8, 0);
-	//	}
-	//}
-
-	//imwrite("result-2.jpg", image);
-	//cout << rvec << endl;
-	//cout << tvec << endl;
-
-	return 0;
-
-}
-
